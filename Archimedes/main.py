5027e5d3d8d4db1f885ed87467b8a9669e1810ad
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-02 19:10:40 +0300

    Started another challange.

---
+++ Archimedes/main.py
@@ -0,0 +1,47 @@
+import pygame
+
+pygame.init()
+
+dims = [650, 650]
+color = [(0, 0, 0), (255, 0, 0), (255, 255, 255)]
+center = [dims[0]/2, dims[1]/2]
+radius = [150, 7]
+start = (center[0], center[1]-radius[0])
+end = (center[0], center[1]+radius[0])
+circlexy = [center[0], center[1]-radius[0]]
+clock = pygame.time.Clock()
+
+
+screen = pygame.display.set_mode(dims)
+pygame.display.set_caption('Archimedes Illision')
+
+def draw_line():
+    pygame.draw.line(screen, color[0], start, end, 4)
+
+def draw_smallcirlce():
+    pygame.draw.circle(screen, color[2], circlexy, radius[1])
+
+run, move = True, True
+factor = 0
+speed = 0.1
+
+while run:
+    screen.fill(color[0])
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    #moving the small circle
+    if circlexy[1] >= start[1] and move == True:
+        circlexy[1] += 1
+        move = False
+    elif circlexy[1] > end[1]:
+        circlexy[1] -= 1
+
+    pygame.draw.circle(screen, color[1], center, radius[0])
+    draw_line()
+    draw_smallcirlce()
+    clock.tick(100)
+    pygame.display.update()
+
+pygame.quit()
